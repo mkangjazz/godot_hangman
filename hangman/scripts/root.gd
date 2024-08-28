0022b014a12b004d75393c9cba70dd8bda0c5ea5
Michael Kang <kaizenmike@gmail.com>
2024-08-28 13:02:28 -0600

    add victory conditions

--- hangman/scripts/root.gd
+++ hangman/scripts/root.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-const blank_space_scene = preload("res://scenes/blank_space.tscn");
+const blank_space_scene:Resource = preload("res://scenes/blank_space.tscn");
+const allowed_strikes:int = 5;
 
 @onready var keyboard_keys_container = %Keyboard
 @onready var blank_spaces_container = %BlankSpaces
+@onready var incorrect_guesses_container = %IncorrectGuesses
 
 # need to switch between index num and char, due to unique values
 var current_word:String = "";
-#var blank_spaces:Array = []; can we just get the children of blankSpaces?
-var player_guesses:Array = [];
+var incorrect_guesses:Array = [];
 var keyboard_keys:Dictionary = {};
+var blank_spaces:Array = [];
 var word_bank:Array = [
 	"apple",
 	"pear",
@@ -19,8 +21,7 @@ var word_bank:Array = [
 	"stack",
 ];
 
-#var allowed_string:String = "abcdefghijklmnopqrstuvwxyz";
-var allowed_string:String = "qwertyuiopasdfghjklzxcvbnm"; # break into rows?
+var allowed_string:String = "qwertyuiopasdfghjklzxcvbnm"; # break into rows? chunk into diff sizes?
 var allowed_chars:Array = [];
 
 func _ready():
@@ -47,6 +48,9 @@ func _input(event):
 func _process(delta):
 	pass
 
+#func add_incorrect_guess():
+	
+
 func set_up_current_word():
 	var random_word:String = get_random_word();
 	
@@ -57,7 +61,7 @@ func set_up_current_word():
 		blank_spaces_container.add_child(space)
 
 		space.setCharacter(char);
-		space.showAnswer();
+		blank_spaces.append(space);
 	pass
 
 func handle_keyboard_keypress(key:String):
@@ -87,7 +91,7 @@ func set_up_new_game():
 	set_up_player_keyboard();
 	set_up_current_word();
 
-	player_guesses = [];
+	incorrect_guesses = [];
 	pass
 
 func get_random_word():
@@ -104,16 +108,55 @@ func get_random_word():
 	pass;
 
 func handle_player_input_submission(key:String):
-	var lower_key = key.to_lower();
+	var lower_key:String = key.to_lower();
+	var found_indexes:Array = [];
 	
 	if !allowed_chars.has(lower_key):
 		return;
-	else: 
-		print("handle_player_input_submission: ", lower_key)
-		#var text:String = line_edit.text;
-		#player_guesses.append(text);
-		#print("player_guesses: ", player_guesses);
-		# when button is clicked
-		# commit the value in the text input as a "guess"
+
+	if current_word.find(lower_key) == -1:
+		var label = Label.new();
+
+		label.text = lower_key.to_upper();
+		incorrect_guesses_container.add_child(label)
+		incorrect_guesses.append(lower_key);
+	else:
+		var i = 0;
+		for char in current_word:
+			if (char == lower_key):
+				found_indexes.append(i);
+			i += 1;
+	
+	for index in found_indexes:
+		blank_spaces[index].showAnswer();
+		pass
+	
+	check_win_lose_conditions();
+
 	pass
 
+func show_win():
+	print("YOU WIN")
+	get_tree().paused = true # or disable inputs and do other things?
+	pass
+
+func show_lose():
+	print("YOU LOSE")
+	get_tree().paused = true # or disable inputs and do other things?
+	pass
+
+func check_win_lose_conditions():
+	if incorrect_guesses.size() >= allowed_strikes:
+		show_lose();
+	else:
+		var all_spaces_revealed = true;
+
+		for space in blank_spaces:
+			if space.revealAnswer == false:
+				all_spaces_revealed = false;
+				break;
+			pass
+
+		if all_spaces_revealed:
+			show_win()
+	pass
